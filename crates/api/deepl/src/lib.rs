655efc18b2eb5b262f722ceb41dad7b5268f56fe
adil <adilahmeddev@gmail.com>
2026-01-16 21:38:53 +0000

    parse URL result wasn't unwrapped

--- crates/api/deepl/src/lib.rs
+++ crates/api/deepl/src/lib.rs
@@ -106,8 +106,7 @@ impl AsyncTranslator for DeeplTranslator {
 pub async fn get_languages(auth: &String) -> anyhow::Result<Vec<String>> {
     let client = Client::new();
 
-    let mut url = Url::parse(get_url(auth))?
-        .join("v2/languages");
+    let mut url = Url::parse(get_url(auth))?.join("v2/languages")?;
     url.set_query(Some("type=source"));
 
     let response = client
